b2b9872dd1e7b87652c4773c206fcfa2fe6e5e8d
KotlinMaestro <servincentparsley@gmail.com>
2023-01-05 15:58:34 +0300

    Typescript types 1

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
 "use strict";
-console.log("Hello");
-let count = 78;
-if (count > 50) {
-    count -= 10;
-}
-console.log(count);
+let sales = 454534334;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+level = 9;
+level = 'Jay';
+let numbers = [1, 4, 6, "string"];
+let numbers2 = [23, 45, 21, 56];
+let names = ["Vince", "John", "Jennifer", "Michelangelo"];
+console.log(names.forEach(n => n.endsWith("e")));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-console.log("Hello")
-let count: number = 78;
-if (count >50){
- count -=10;
-}
-console.log(count);
+let sales: number = 454_534_334/*using underscore to huge numbers for readability */
+let course: string  = 'TypeScript'
+let is_published: boolean = true
+/* variables with the type not defined
+TypeScript will infer from the assigned value */
+let level
+/*Variables without initializations and no type defined will be 
+infered to be of type 'any' */
+
+/* the ANY type*/
+/* As aforementioned, can be used to avoid
+defining the data type of a variable, or it is the 
+default for variables that are not initialized */
+//example.
+level = 9
+level = 'Jay'
+//This makes it possible to change the type of values of data we assign to values. A bad practice.
+/*Arrays*/
+let numbers = [1,4,6, "string"] //dynamic javascript array
+
+let numbers2 :number[] = [23,45,21,56]//TS arrays that are confined to the type
+let names:string[] = ["Vince", "John", "Jennifer", "Michelangelo"]
+
+/*TS also offers autocompletion of all the possible actions you can perform on data*/
+console.log(names.forEach( n =>n.endsWith("e")))
