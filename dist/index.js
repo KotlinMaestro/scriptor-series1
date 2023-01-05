635a41e4c3ed85a0a41855eda286f342f043bb44
KotlinMaestro <servincentparsley@gmail.com>
2023-01-05 11:01:21 +0300

    config and index files

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello");
+let count = 78;
+if (count > 50) {
+    count -= 10;
+}
+console.log(count);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello")
+let count: number = 78;
+if (count >50){
+ count -=10;
+}
+console.log(count);
